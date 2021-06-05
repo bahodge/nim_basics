223c81ddba55d93a0c778eaac843cce7acd33f09
Ben Hodge <benjamin.andrew.hodge@gmail.com>
2021-06-05 13:08:25 -0500

    adding conditionals

---
+++ case.nim
@@ -0,0 +1,10 @@
+const x = 11
+
+case x
+of 5:
+  echo "Five!"
+of 7:
+  echo "Seven!"
+of 10:
+  echo "Ten!"
+else: discard
---
+++ else.nim
@@ -0,0 +1,13 @@
+let 
+  d = 63
+  e = 2.718
+
+if d < 10:
+  echo "d is small"
+else:
+  echo "d is large"
+
+if e < 10:
+  echo "e is small"
+else:
+  echo "e is large"
